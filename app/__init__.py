24ab0c5952685f49b6a5fd063a5886849da86c1a
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-07-05 19:20:04 +0300

    Project preparing.

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+"""
+Бот с музыкой из Вконтакте.
+"""
+__author__ = 'Kirill Lapushinskiy, Nikita Novikov'
+__maintainer__ = __author__
+
+__email__ = 'kirill@lapushinskiy.ru'
+__license__ = 'GNU Affero General Public License v3.0'
+__version__ = '0.0.1'
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
