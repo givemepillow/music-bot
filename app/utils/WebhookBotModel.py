826d6e0d814347899c12a6f0707ac2d370f9632c
caVenikk <sqqvt1@gmail.com>
2022-07-13 18:45:13 +0300

    Environ variables usage

--- app/utils/WebhookBotModel.py
+++ app/utils/WebhookBotModel.py
@@ -1,13 +1,15 @@
-from .AbstractBotModel import AbstractBotModel
-from aiogram.utils.executor import start_webhook
+import os
 
+from aiogram.utils.executor import start_webhook
 from loguru import logger
+
+from .AbstractBotModel import AbstractBotModel
 
 
 class WebhookModel(AbstractBotModel):
     async def on_startup(self, _dispatcher):
         await super().on_startup(_dispatcher)
-        await self._bot.set_webhook(self.config.webhook.host + self.config.webhook.path)
+        await self._bot.set_webhook(os.environ['WEBHOOK_HOST'] + os.environ['WEBHOOK_PATH'])
 
     async def on_shutdown(self, _dispatcher):
         await super().on_shutdown(_dispatcher)
@@ -17,10 +19,10 @@ class WebhookModel(AbstractBotModel):
         logger.warning("The application is running in webhook mode.")
         start_webhook(
             dispatcher=self._dispatcher,
-            webhook_path=self.config.webhook.path,
+            webhook_path=os.environ['WEBHOOK_PATH'],
             on_startup=self.on_startup,
             on_shutdown=self.on_shutdown,
             skip_updates=True,
-            host=self.config.webapp.host,
-            port=self.config.webapp.port,
+            host=os.environ['WEBAPP_HOST'],
+            port=os.environ['WEBAPP_PORT'],
         )
