81e5dac77fcd7e3f731b0cf79449f580edff06e5
caVenikk <sqqvt1@gmail.com>
2022-07-18 17:30:36 +0300

    New state local_searching

--- app/core/states.py
+++ app/core/states.py
@@ -4,4 +4,4 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class States(StatesGroup):
     searching = State()
     setting_link = State()
-    checking_audios = State()
+    local_searching = State()
