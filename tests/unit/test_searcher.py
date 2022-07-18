d9d2f532929bcda2fb389c2f49c3dd2c5af50f56
caVenikk <sqqvt1@gmail.com>
2022-07-18 17:31:38 +0300

    Async tests (searchers now have async method __call__)

--- tests/unit/test_searcher.py
+++ tests/unit/test_searcher.py
@@ -4,9 +4,10 @@ from aiovkmusic import Track
 from app.core.services.search import MusicSearcher
 
 
-def test_search_anything(fake_music):
+@pytest.mark.asyncio
+async def test_search_anything(fake_music):
     searcher = MusicSearcher(music=fake_music, step=5, pages=5)
-    searcher[88]('anything')
+    await searcher[88]('anything')
     assert len(searcher[88].next()) == 5
     assert len(searcher[88].prev()) == 5
 
@@ -19,18 +20,20 @@ def test_search_without_query(fake_music):
     assert len(searcher[99].prev()) == 0
 
 
-def test_search_step(fake_music):
+@pytest.mark.asyncio
+async def test_search_step(fake_music):
     searcher1 = MusicSearcher(music=fake_music, step=3, pages=5)
-    searcher1[12]('anything')
+    await searcher1[12]('anything')
     assert len(searcher1[12].next()) == 3
     searcher2 = MusicSearcher(music=fake_music, step=30, pages=5)
-    searcher2[22]('anything')
+    await searcher2[22]('anything')
     assert len(searcher2[22].next()) == 30
 
 
-def test_search_history(fake_music):
+@pytest.mark.asyncio
+async def test_search_history(fake_music):
     searcher = MusicSearcher(music=fake_music, step=3, pages=5)
-    searcher[100]('anything')
+    await searcher[100]('anything')
     assert len(searcher[100].next()) == 3
     assert type(searcher[100].track(999999999)) is Track
     with pytest.raises(KeyError):
