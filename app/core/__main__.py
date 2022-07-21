c3133855e3091d7e77d84fd49c5d811c1252485f
caVenikk <sqqvt1@gmail.com>
2022-07-21 22:49:01 +0300

    Exception handling

--- app/core/__main__.py
+++ app/core/__main__.py
@@ -1,6 +1,7 @@
 import sys
 import warnings
 
+import aiohttp.http_exceptions
 import loguru
 
 from app.core.handlers import dp as dispatcher
@@ -10,7 +11,7 @@ warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 loguru.logger.remove()
 loguru.logger.add(
     sink=sys.stdout,
-    format="{time:MMM DD HH:mm:ss.SSS}: {message}",
+    format="{level} {time:MMM DD HH:mm:ss.SSS}: {message}",
     enqueue=True
 )
 
@@ -21,4 +22,7 @@ if __name__ == '__main__':
     loguru.logger.info(
         f"Number of callback query handlers: {len(dispatcher.callback_query_handlers.handlers)}."
     )
-    bot_engine.start()
+    try:
+        bot_engine.start()
+    except aiohttp.http_exceptions.BadStatusLine:
+        loguru.logger.warning('aiohttp response 400: Invalid method encountered')
