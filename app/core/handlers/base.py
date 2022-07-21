588e8351cac33a541c9540008533dd6cc8174155
caVenikk <sqqvt1@gmail.com>
2022-07-21 22:49:25 +0300

    Playlists instance

--- app/core/handlers/base.py
+++ app/core/handlers/base.py
@@ -1,5 +1,6 @@
 from app.core.loader import music
-from app.core.services import MusicSearcher
+from app.core.services import MusicSearcher, Playlists
 
 searcher = MusicSearcher(music, 7, 5)
 local_searcher = MusicSearcher(step=7, pages=5, local=True)
+playlists = Playlists()
