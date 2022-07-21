f97853f6f560fdf765fbe6b4427846906e580d60
caVenikk <sqqvt1@gmail.com>
2022-07-21 22:54:26 +0300

    Registration of new handlers + refactor

--- app/core/handlers/__init__.py
+++ app/core/handlers/__init__.py
@@ -1,8 +1,10 @@
+from .commands import dp
 from .local_search import dp
 from .music_search import dp
 from .music_send import dp
+from .playlist_tracks import dp
 from .plug import dp
+from .profile import dp
 from .set_link import dp
-from .start import dp
 
 __all__ = ['dp']
